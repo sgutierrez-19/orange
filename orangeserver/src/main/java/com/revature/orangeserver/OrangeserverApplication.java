89a85c92a96807c7665617fcfde4aac194868396
Steven Gutierrez <stematgut@gmail.com>
2020-07-27 10:29:11 -0700

    Created basic spring project and set up yml file for H2

---
+++ orangeserver/src/main/java/com/revature/orangeserver/OrangeserverApplication.java
@@ -0,0 +1,13 @@
+package com.revature.orangeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeserverApplication.class, args);
+	}
+
+}
---
+++ orangeserver/src/test/java/com/revature/orangeserver/OrangeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.orangeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
